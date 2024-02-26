ccb4fec55ef1f84fdf264fa8abeed5b8a46e6505
16mossm <50373533+16mossm@users.noreply.github.com>
2024-02-26 13:49:49 +0000

    fixed die class

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,24 +8,30 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
+        
+        private int value;      //current value 
+        private int maxValue;   // the maximum value the dice can be 
 
-        //Property
-        private int value;
-
-        //Method
+        
         public int roll()
         {
             Random rd = new Random();
-            this.value = rd.Next(1, 7); // lowwer bound is inclusive; upper bound is exclusive 
+            this.value = rd.Next(1, maxValue + 1); // lowwer bound is inclusive; upper bound is exclusive 
             return this.value;
         }
 
         public int getValue() {
             return this.value;
+        }
+
+
+        public Die(int maxValue = 6)    //made an initaliser so that the class is more adaptable 
+        {
+            if(maxValue < 1)    //dice cannot have less than 1 side
+            {
+                throw new ArgumentOutOfRangeException("should be 1 or higher"); //throws error
+            }
+            this.maxValue = maxValue;
         }
 
     }
