20351ba2b47ca93e3c5d9fb147d85c6ebff83460
16mossm <50373533+16mossm@users.noreply.github.com>
2024-02-26 14:22:39 +0000

    made game class

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,14 +8,58 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
+        Die[] rolls; // the dice that the game rolls
+        
+        public Game(int ammountOfRolls = 3, int dieSize = 6)  
+        {
+            this.rolls = new Die[ammountOfRolls];
+            for(int i = 0; i < ammountOfRolls; i++)
+            {
+                this.rolls[i] = new Die(dieSize);   //initialise all of the dice
+            }
+        }
 
-        //Methods
+
+        public int GetSum() //returns the sum of all dice
+        {
+            int total = 0;
+            for (int i = 0; i < this.rolls.Length; i++)  //if -1 roll all of the dice
+            {
+                total = total + this.GetRoll(i);
+            }
+            return total;
+        }
+
+        public void rollDice(int die = -1)   //rolles specified die. if -1 it rolls all of them
+        {
+            if(die == -1)
+            {
+                for(int i = 0; i < this.rolls.Length; i++)  //if -1 roll all of the dice
+                {
+                    this.rolls[i].roll();
+                    
+                }
+                return;
+            }
+                
+            if (die < 0 || die >= this.rolls.Length)    // if out of range throw an exeption
+            {
+                throw new IndexOutOfRangeException("index out of range");
+            }
+
+            this.rolls[die].roll();     //roll the specified die
+
+        }
+
+
+        public int GetRoll(int index)
+        {
+            if(index < 0 || index >= this.rolls.Length)
+            {
+                throw new IndexOutOfRangeException("index out of range");
+            }
+            return this.rolls[index].getValue();    //returns the value of the dice specified.
+        }
 
     }
 }
