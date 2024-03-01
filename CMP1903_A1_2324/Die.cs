bacdfb32d761387faa69f6c5ec530c30d498b5d1
16mossm <50373533+16mossm@users.noreply.github.com>
2024-03-01 18:51:28 +0000

    added xml comments

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -12,19 +12,29 @@ namespace CMP1903_A1_2324
         private int value;      //current value 
         private int maxValue;   // the maximum value the dice can be 
 
-        
-        public int roll()
+        /// <summary>
+        /// rolles the die and gets a value from 1 to maxvalue (inclusive)
+        /// </summary>
+        /// <returns>the value rolled</returns>
+        public int Roll()
         {
             Random rd = new Random();
             this.value = rd.Next(1, maxValue + 1); // lowwer bound is inclusive; upper bound is exclusive 
             return this.value;
         }
 
-        public int getValue() {
+        /// <summary>
+        /// gets the last value rolled
+        /// </summary>
+        /// <returns>the last value rolled</returns>
+        public int GetValue() {
             return this.value;
         }
 
-
+        /// <summary>
+        /// intialises the class
+        /// </summary>
+        /// <param name="maxValue">the maximum value that the die can roll</param>
         public Die(int maxValue = 6)    //made an initaliser so that the class is more adaptable 
         {
             if(maxValue < 1)    //dice cannot have less than 1 side
--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -11,6 +11,11 @@ namespace CMP1903_A1_2324
         Die[] rolls; // the dice that the game rolls
         int DiceRolled = -1;    // the index of the last dice rolled (for the continuous rolling)
 
+        /// <summary>
+        /// rolles the specified ammount of dice.
+        /// </summary>
+        /// <param name="ammountOfRolls">the ammount of dice rolled</param>
+        /// <param name="dieSize">the maxSize of each of the dice</param>
         public Game(int ammountOfRolls = 3, int dieSize = 6)  
         {
             this.rolls = new Die[ammountOfRolls];
@@ -20,12 +25,17 @@ namespace CMP1903_A1_2324
             }
         }
 
+        /// <summary>
+        ///rolls the next available die.
+        ///throws exception if all available dice have been rolled
+        /// </summary>
+        /// <returns>the value of the die rolled</returns>
         public int RollNext()   //rolls the next available dice;
         {
             try
             {
                 this.rollDice(++DiceRolled);    //rolls the next dice
-                return this.rolls[DiceRolled].getValue();   //returnes the value
+                return this.rolls[DiceRolled].GetValue();   //returnes the value
             }
             catch (IndexOutOfRangeException)
             {
@@ -34,16 +44,24 @@ namespace CMP1903_A1_2324
             }
         }
 
+        /// <summary>
+        /// adds the values of all dice
+        /// </summary>
+        /// <returns>the sum of all dice</returns>
         public int GetSum() //returns the sum of all dice
         {
             int total = 0;
-            for (int i = 0; i < this.rolls.Length; i++)  //if -1 roll all of the dice
+            for (int i = 0; i < this.rolls.Length; i++)  
             {
                 total = total + this.GetRoll(i);
             }
             return total;
         }
 
+        /// <summary>
+        /// rolles the specified die rolls all if -1 is given
+        /// </summary>
+        /// <param name="die">the die to roll, rolls all if -1</param>
         public void rollDice(int die = -1)   //rolles specified die. if -1 it rolls all of them
         {
             this.DiceRolled = this.rolls.Length;    //disables the continuous rolls function.
@@ -51,7 +69,7 @@ namespace CMP1903_A1_2324
             {
                 for(int i = 0; i < this.rolls.Length; i++)  //if -1 roll all of the dice
                 {
-                    this.rolls[i].roll();
+                    this.rolls[i].Roll();
                     
                 }
                 return;
@@ -62,17 +80,21 @@ namespace CMP1903_A1_2324
                 throw new IndexOutOfRangeException("index out of range");
             }
 
-            this.rolls[die].roll();     //roll the specified die
+            this.rolls[die].Roll();     //roll the specified die
         }
 
-
+        /// <summary>
+        /// gets the value of the specified die
+        /// </summary>
+        /// <param name="index">the die to get the value of</param>
+        /// <returns>the value of the specified die</returns>
         public int GetRoll(int index)
         {
             if(index < 0 || index >= this.rolls.Length)
             {
                 throw new IndexOutOfRangeException("index out of range");
             }
-            return this.rolls[index].getValue();    //returns the value of the dice specified.
+            return this.rolls[index].GetValue();    //returns the value of the dice specified.
         }
 
     }
